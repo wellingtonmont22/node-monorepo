f0e15234c84ce92fd2d6dce854086dc78277d809
Wellington Monteiro <wellington.m.alves@hotmail.com>
2024-03-04 22:03:14 -0300

    criando pacote e testando.

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,5 +1,7 @@
 import app from "./app";
 import { env } from "../env";
+import { mostraMensagem } from 'utils-string'
 
+console.log(mostraMensagem())
 
 app.listen(env.PORT, () => console.log(`Running in ${env.PORT}`))
---
+++ packages/utils-string/index.js
@@ -0,0 +1,8 @@
+/**
+ * mostra mensagem
+ * @returns {string}
+ */
+
+export const mostraMensagem = function () {
+    return 'Monorepo é show!!'
+}
